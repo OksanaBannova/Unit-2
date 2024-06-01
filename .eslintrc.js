0057f7701d3620c762596f9b9aaf773ed631cb3d
Oksana Bannova <oksanchik2170@yandex.ru>
2024-06-01 19:02:59 +0700

    readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [],
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    requireConfigFile: false,
-  },
-  rules: {
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: 'eslint:recommended',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {},
 };
---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/ctn5k8spj3qqt8ww?svg=true)](https://ci.appveyor.com/project/OksanaBannova/unit-2-ko1fw)
